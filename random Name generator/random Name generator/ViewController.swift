cc829641b74aa60d3ea95faa271b379f033fab4d
Charan0517 <sricharanv2001@gmail.com>
2023-09-25 23:56:53 -0500

    Added project

---
+++ random Name generator/random Name generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  random Name generator
+//
+//  Created by Sri Charan on 25/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
