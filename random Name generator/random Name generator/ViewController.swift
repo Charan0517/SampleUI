2c230bdec3a798004b51942883b808c8efb9d8b5
Charan0517 <sricharanv2001@gmail.com>
2023-09-26 01:02:27 -0500

    Playued with images

--- random Name generator/random Name generator/ViewController.swift
+++ random Name generator/random Name generator/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let Array = ["Tiger", "Lion", "Elephant", "Wolf"]
+    var set: Set<String> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBOutlet weak var image: UIImageView!
+    
 
+    
+    @IBAction func GeneratePhoto(_ sender: UIButton) {
+        var s = Array[Int.random(in: 0..<Array.count)]
+        if !set.contains(s) {
+            image.image = UIImage(named: s)
+            set.insert(s)
+        }
+        if s.count == Array.count{
+            set = []
+        }
+        
+    }
+    
 }
 
